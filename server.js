00eb7026368cd0d71db24525d85d73f6c417f1f0
baoyong <545009168@qq.com>
2018-01-30 18:36:05 +0800

    by

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.prepare()
 
     server.listen(8000, (err) => {
         if(err) throw err;
-        console.log('> Ready on http://localhost:3000');
+        console.log('> Ready on http://localhost:8000');
     })
 })
     .catch((ex) => {
